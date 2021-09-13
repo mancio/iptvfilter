68a39dd9902b6ede1dfa3055080654f0e24c406b
Andrea Mancini <mancioman2@gmail.com>
2021-09-13 17:28:32 +0200

    check if source file exist

--- operations.py
+++ operations.py
@@ -1,23 +1,27 @@
 from os import path
 from datetime import datetime, timedelta
+from pathlib import Path
+
 import requests
 
 import inputs
 
 
 def is_file_old(h):
-    file_date = datetime.fromtimestamp(path.getmtime(inputs.input_file))
-    hours_ago = datetime.now() - timedelta(hours=h)
-    print(file_date)
-    print(hours_ago)
-    return file_date <= hours_ago
+    if Path(inputs.input_file).is_file():
+        file_date = datetime.fromtimestamp(path.getmtime(inputs.input_file))
+        hours_ago = datetime.now() - timedelta(hours=h)
+        print(file_date)
+        print(hours_ago)
+        return file_date <= hours_ago
+    else:
+        return bool(True)
 
 
 # download m3u original file
 def m3u_download(url, file):
     r = requests.get(url, allow_redirects=True)
     open(file, 'wb').write(r.content)
-    file.close()
     return file
 
 
